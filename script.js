e977b69456cbf011369e52517e62c761e58b68e1
Masood-Haider <masood.haider.bangash1@gmail.com>
2024-07-25 20:32:00 +0500

    h

---
+++ script.js
@@ -0,0 +1,84 @@
+// Assignment 1:
+// Task: Write a JavaScript function that takes two numbers as arguments and returns their sum.
+// Function Name: addNumbers
+
+// function addNumbers(num1,num2){
+//     let a=num1+num2;
+//     console.log(a);
+// }
+// addNumbers(2,4);
+
+// Assignment 2:
+// Task: Write a JavaScript function that takes a string as an argument and returns the string in uppercase.
+
+// function toUpper(str){
+//     let a=str.toUpperCase();
+//     console.log(a);
+// }
+// toUpper("hello");
+
+
+// Assignment 3:
+// Task: Write a JavaScript function that takes an array of numbers as an argument and returns the average of the numbers.
+
+// function averageArray(arr){
+//     let a=0;
+//   for(i=0;i<arr.length;i++){
+//     a=a+arr[i];
+
+//   }
+//   let b=a/arr.length;
+//   console.log(b);
+// }
+// let c=[1,2,3,4,5,6,7,3,4,5,3,5,6,4];
+// averageArray(c);
+
+
+
+// assignment 4:
+// Task: Write a JavaScript function that takes a string as an argument and returns the string with all vowels removed.
+
+// function removeVowels(str){
+//     let vows = "aeiouAEIOU";
+//     let a = "";
+      
+//     for (let i = 0; i < str.length; i++) {
+//           if (vows.indexOf(str[i]) === -1) {
+//             a=a+str[i];
+//       }
+//     }
+//  console.log(a);     
+// }
+// g="hello world";
+// removeVowels(g);
+
+
+
+// Assignment 5:
+// Task: Write a JavaScript function that takes a number as an argument and returns the factorial of that number.
+
+// function fact(num){
+//     if (num==1 || num==0){
+//         return 1;
+//     }
+//     else{
+//        return num*fact(num-1);
+//     }
+    
+// }
+// console.log(fact(7));
+
+
+// Assignment 6:
+// Task: Write a JavaScript function that takes a string as an argument and returns the string with all letters reversed.
+
+
+// function reversed(str){
+//     let st="";
+//     for(i=str.length-1;i>=0;i--){
+//         st=st+str[i];
+//      }
+//      console.log(st);
+// }
+// let g="hello world";
+// reversed(g);
